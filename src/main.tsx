043351c1d165b7b0953af74034b197c532ec067a
Dong-Young Kim <31337.persona@gmail.com>
2022-06-15 02:44:52 +0900

    build: add `recoil` to dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 
 import ReactDOM from "react-dom";
+import { RecoilRoot } from "recoil";
 
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
   </React.StrictMode>,
   document.getElementById("root"),
 );
