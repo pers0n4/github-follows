dae237af19839278dd09d3cb0dde61e2d46e1d8f
Dong-Young Kim <31337.persona@gmail.com>
2022-06-16 21:03:46 +0900

    feat: add PagePane to Layout when user authorized

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,9 +3,14 @@ import React from "react";
 import { Box, PageLayout } from "@primer/react";
 import { Outlet } from "react-router-dom";
 
+import { useToken } from "~/recoil/auth";
+
 import PageHeader from "./PageHeader";
+import PagePane from "./PagePane";
 
 function Layout() {
+  const token = useToken();
+
   return (
     <Box bg="canvas.default" minHeight="100vh">
       <PageHeader />
@@ -13,6 +18,11 @@ function Layout() {
         <PageLayout.Content>
           <Outlet />
         </PageLayout.Content>
+        {token && (
+          <PageLayout.Pane position="start" width="small" divider="line">
+            <PagePane />
+          </PageLayout.Pane>
+        )}
       </PageLayout>
     </Box>
   );
