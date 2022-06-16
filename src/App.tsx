1fa4ded7508212d62e2c8b66559993728358b4d6
Dong-Young Kim <31337.persona@gmail.com>
2022-06-17 02:04:53 +0900

    feat: add `Dashboard` page

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import React from "react";
 import { BaseStyles, ThemeProvider } from "@primer/react";
 import { Routes, Route } from "react-router-dom";
 
+import AuthLayout from "~/components/Auth/AuthLayout";
 import Layout from "~/components/Layout";
 import Authorize from "~/pages/Authorize";
+import Dashboard from "~/pages/Dashboard";
 import Home from "~/pages/Home";
 
 function App() {
@@ -15,6 +17,9 @@ function App() {
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
             <Route path="authorize" element={<Authorize />} />
+          </Route>
+          <Route path="/dashboard" element={<AuthLayout />}>
+            <Route index element={<Dashboard />} />
           </Route>
         </Routes>
       </BaseStyles>
