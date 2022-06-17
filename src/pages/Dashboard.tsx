5dd9f87c4677426d15333d505fc56855316f16d9
Dong-Young Kim <31337.persona@gmail.com>
2022-06-18 03:15:17 +0900

    feat: implement `Dashboard` page

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,69 @@
 import React from "react";
 
+import { Box, Button } from "@primer/react";
+
+import FollowsBox from "~/components/FollowsBox";
+import {
+  useFilteredFollows,
+  useFollowsActions,
+  useFollowsTypes,
+} from "~/recoil/follows";
+
 function Dashboard() {
-  return <div>Dashboard</div>;
+  const [filter] = useFollowsTypes();
+  const [fillteredFollows, setFillteredFollows] = useFilteredFollows();
+  const { fetchFollows, follow, unfollow } = useFollowsActions();
+
+  React.useEffect(() => {
+    fetchFollows(); // eslint-disable-line @typescript-eslint/no-floating-promises
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  const createButton = (id: string) => {
+    switch (filter) {
+      case "Followers":
+        return (
+          <Button
+            variant="outline"
+            onClick={async () => {
+              await follow(id);
+              setFillteredFollows(
+                fillteredFollows.filter((item) => item.id !== id),
+              );
+            }}
+          >
+            Follow
+          </Button>
+        );
+      case "Following":
+        return (
+          <Button
+            variant="danger"
+            onClick={async () => {
+              await unfollow(id);
+              setFillteredFollows(
+                fillteredFollows.filter((item) => item.id !== id),
+              );
+            }}
+          >
+            Unfollow
+          </Button>
+        );
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <Box display="grid" gridTemplateColumns="1fr" gridRowGap={2}>
+      {fillteredFollows.map((filteredFollow) => (
+        <FollowsBox
+          user={filteredFollow}
+          key={filteredFollow.id}
+          actionButton={createButton(filteredFollow.id)}
+        />
+      ))}
+    </Box>
+  );
 }
 
 export default Dashboard;
